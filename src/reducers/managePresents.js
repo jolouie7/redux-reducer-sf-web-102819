c5eefc1562770c5c11e7bad7c431d5466eb1e9e0
Your_Name <joseph161216@gmail.com>
2020-01-26 12:28:51 -0800

    finished

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,29 @@
-export function managePresents(state, action){
-}
+// export function managePresents(state = {
+//    numberOfPresents: 0,
+//    }, action) {
+//     swtich(action.type) {
+
+//     case "INCREASE":
+//       return { numberOfPresents: state.numberOfPresents + 1 };
+//     default:
+//       return state
+//   }
+// }
+
+export function managePresents(state = {
+  numberOfPresents: 0,
+}, action) {
+  switch(action.type) {
+
+    case "INCREASE":
+      // return Object.assign({}, state, {
+      //   numberOfPresents: state.numberOfPresents + 1
+      // });
+      return (
+         {...state, numberOfPresents: state.numberOfPresents + 1}
+      )
+
+    default:
+      return state;
+  }
+};
